6c4224a43c5c1fd039ce0450b1e07547c81c1873
Theosimon0828 <theod2823@gmail.com>
2021-12-05 18:57:23 +0100

    3rd commit

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -9,13 +9,13 @@ const Carousel = () => {
         
         <div id="carouselExampleSlidesOnly" className="carousel slide pwrapper" data-bs-ride="carousel">
             <div className="carousel-inner">
-                <div className="carousel-item active picture" data-bs-interval="2000">
+                <div className="carousel-item active picture" data-bs-interval="1500">
                     <img src={photo1} className="d-block w-100" alt="street"/>
                 </div>
-                <div className="carousel-item picture" data-bs-interval="2000">
+                <div className="carousel-item picture" data-bs-interval="1500">
                     <img src={photo2} className="d-block w-100" alt="..."/>
                 </div>
-                <div className="carousel-item picture" data-bs-interval="2000">
+                <div className="carousel-item picture" data-bs-interval="1500">
                     <img src={photo3} className="d-block w-100" alt="..."/>
                 </div>
             </div>
